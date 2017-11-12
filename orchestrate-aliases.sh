34f1ac786bcb454e8b219941bc515c8731637063
piotr@desktop-mint <piotrkrzysztofkozak@gmail.com>
2017-11-12 09:24:26 +0100

    Add xclip alias

--- orchestrate-aliases.sh
+++ orchestrate-aliases.sh
@@ -7,5 +7,6 @@ alias dsa='docker ps -q | xargs docker stop'
 alias dra='docker ps -a -q | xargs docker rm'
 alias dria='docker images -q | xargs docker rmi -f'
 alias dris='docker images | grep SNAPSHOT | awk '\''{print \$3}'\'' | xargs docker rmi -f'
+alias cclip='xclip -selection clipboard'
 EOL
 . $rc
