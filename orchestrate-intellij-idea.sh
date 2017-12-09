d58594fabc5bb20b797552f8f7e1e74bd27876c8
piotr@desktop-mint <piotrkrzysztofkozak@gmail.com>
2017-12-09 09:43:04 +0100

    Ask if configure intellij idea

--- orchestrate-intellij-idea.sh
+++ orchestrate-intellij-idea.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-# create pf dir
-existingDirs=$(ls -d $HOME/.IntelliJIdea* $HOME/.intellij-idea* 2>/dev/null)
-echo "Existing Intellij Idea directories:"
-echo "$existingDirs"
-echo -e -n "${CYAN}Enter path to intellij home directory${NC}: "
-read intellijHomeDir
-makeDir $intellijHomeDir
+configureIntellijDefault="n"
+echo -e -n "${CYAN}Configure intellij idea settings [y/n]${NC} ($configureIntellijDefault): "
+read configureIntellij
+configureIntellij=${configureIntellij:-$configureIntellijDefault}
 
-. intellij-idea/jdk-table.sh
+if [ "$configureIntellij" == "y" ]; then
+
+	existingDirs=$(ls -d $HOME/.IntelliJIdea* $HOME/.intellij-idea* 2>/dev/null)
+	echo "Existing Intellij Idea directories:"
+	echo "$existingDirs"
+	echo -e -n "${CYAN}Enter path to intellij home directory${NC}: "
+	read intellijHomeDir
+	makeDir $intellijHomeDir
+	. intellij-idea/jdk-table.sh
+
+fi;
