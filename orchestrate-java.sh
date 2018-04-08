70a7c20b359ad8560e515081b2645d96be79ded4
pkk82@macbook-pro <piotrkrzysztofkozak@gmail.com>
2018-04-08 11:32:48 +0200

    Modify java approach for Mac OS after releasing version 9 and 10

--- orchestrate-java.sh
+++ orchestrate-java.sh
@@ -16,17 +16,23 @@ done
 
 for javaDmg in `ls -d $cloudDir/java/$system/*.dmg 2>/dev/null`; do
 	mountDir=`hdiutil attach $javaDmg | awk 'FNR==2{print substr($0, index($0, $3))}'`;
-	pkgFile=`find "${mountDir}" -name "*.pkg" 2>/dev/null | head -n 1`
-	majorVersion=`echo $javaDmg | awk -F- '{print $(NF-2)}' | cut -d'u' -f1`
-	patchVersion=`echo $javaDmg | awk -F- '{print $(NF-2)}' | cut -d'u' -f2`
-	destFolder=jdk-1.${majorVersion}.0_${patchVersion}-x64
+	mainPkgFile=`find "${mountDir}" -name "*.pkg" 2>/dev/null | head -n 1`
+	version=`echo $javaDmg | awk -F- '{print $(NF-2)}'`
+	majorVersion=`echo $version | awk -Fu '{print $1}'`
+	patchVersion=`echo $version | awk -Fu '{print $2}'`
+	if [ "$patchVersion" == "" ]; then
+		destFolder=jdk-${majorVersion}-x64
+	else
+		destFolder=jdk-${majorVersion}_${patchVersion}-x64
+	fi
 	if [ -d "$javaDir/$destFolder" ]; then
 		echo -e "${CYAN}Dir $destFolder exists - skipping${NC}"
 	else
 		rm -rf /tmp/$destFolder
 		rm -rf /tmp/$destFolder-unzipped
-		pkgutil --expand "$pkgFile" /tmp/$destFolder
-		payloadFile=/tmp/$destFolder/jdk1${majorVersion}0${patchVersion}.pkg/Payload
+		pkgutil --expand "$mainPkgFile" /tmp/$destFolder
+		jdkPkgFile=`find "/tmp/$destFolder" -name "jdk*.pkg" 2>/dev/null | head -n 1`
+		payloadFile="${jdkPkgFile}/Payload"
 		mkdir /tmp/$destFolder-unzipped
 		mkdir "$javaDir/$destFolder"
 		tar -zxf $payloadFile -C /tmp/$destFolder-unzipped
