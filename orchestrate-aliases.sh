e93f993ea3429192ce2e1234a3ade7f38462a485
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-04-21 13:03:12 +0200

    Stop loading .bashrc

--- orchestrate-aliases.sh
+++ orchestrate-aliases.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-rc=~/.bashrc
 aliases=~/.bash_aliases
 
 cat > $aliases << EOL
@@ -9,4 +8,3 @@ alias dria='docker images -q | xargs docker rmi -f'
 alias dris='docker images | grep SNAPSHOT | awk '\''{print \$3}'\'' | xargs docker rmi -f'
 alias cclip='xclip -selection clipboard'
 EOL
-. $rc
