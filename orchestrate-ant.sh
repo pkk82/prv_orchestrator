2fee26fada6a2a06a1e0e3c4bfb2d284d94b7e71
pkk82@macbook-pro <piotrkrzysztofkozak@gmail.com>
2018-04-21 13:57:15 +0200

    Fix ant verification

--- orchestrate-ant.sh
+++ orchestrate-ant.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 unzipFamily apache-ant
+# to make verification work
+unset ANT_HOME
 verify apache-ant "bin/ant -version | awk '{print \$4}'" "if [ \$minor -lt \"10\" ] && [ ! -z \$JAVA5_x64_HOME ]; then JAVA_HOME=\$JAVA5_x64_HOME; elif [ \$minor -lt \"10\" ]; then JAVA_HOME=\$JAVA7_x64_HOME; else JAVA_HOME=\$JAVA8_x64_HOME; fi;"
 createVariables2 apache-ant ANT
-echo $JAVA_HOME
