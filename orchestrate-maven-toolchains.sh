d3e0e17f3111041d5723ccc4c9f63c1c59078f9d
pkk82@macbook-pro <piotrkrzysztofkozak@gmail.com>
2018-04-23 20:36:15 +0200

    Change formatting

--- orchestrate-maven-toolchains.sh
+++ orchestrate-maven-toolchains.sh
@@ -15,30 +15,30 @@ EOL
 javaDir=$pfDir/java
 for specJava in `ls -d $javaDir/*`; do
 
-	javaVersion=$(echo $specJava | awk -F- '{print $(NF-1)}' | tr 'u' '_')
-	platform=$(echo $specJava | awk -F- '{print $NF}')
-	major=$(echo $javaVersion | cut -d'.' -f1)
-	minor=$(echo $javaVersion | cut -d'.' -f2)
+  javaVersion=$(echo $specJava | awk -F- '{print $(NF-1)}' | tr 'u' '_')
+  platform=$(echo $specJava | awk -F- '{print $NF}')
+  major=$(echo $javaVersion | cut -d'.' -f1)
+  minor=$(echo $javaVersion | cut -d'.' -f2)
 
-	if [ "$system" == "windows" ]; then
-		finalPath=$(echo $specJava | sed 's|/c|c:|g' | sed 's|/|\\|g')
-	else
-		finalPath=$specJava
-	fi
+  if [ "$system" == "windows" ]; then
+    finalPath=$(echo $specJava | sed 's|/c|c:|g' | sed 's|/|\\|g')
+  else
+    finalPath=$specJava
+  fi
 
 
 cat >> $toolchains << EOL
-	<toolchain>
-		<type>jdk</type>
-		<provides>
-			<version>${major}.${minor}</version>
-			<vendor>sun</vendor>
-			<platform>$platform</platform>
-		</provides>
-		<configuration>
-			<jdkHome>$finalPath</jdkHome>
-		</configuration>
-	</toolchain>
+    <toolchain>
+        <type>jdk</type>
+        <provides>
+            <version>${major}.${minor}</version>
+            <vendor>sun</vendor>
+            <platform>$platform</platform>
+        </provides>
+        <configuration>
+            <jdkHome>$finalPath</jdkHome>
+        </configuration>
+    </toolchain>
 EOL
 
 done
