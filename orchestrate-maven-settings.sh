65b8a949bfedd2ba3a0010392488e42bd7f3eb5a
pkk82@macbook-pro <piotrkrzysztofkozak@gmail.com>
2018-04-23 20:23:18 +0200

    Remove alibaba cloud artifactory from maven config

--- orchestrate-maven-settings.sh
+++ orchestrate-maven-settings.sh
@@ -36,10 +36,6 @@ EOL
 	read -s ossSonatypePassword
 	encryptedOssSonatypePassword=$($MVN_HOME/bin/mvn --encrypt-password $ossSonatypePassword)
 
-	echo -e -n "\n${CYAN}Artifactory (pkk82@alibaba.cloud) password: ${NC}"
-	read -s artifactoryCloudPassword
-	encryptedArtifactoryCloudPassword=$($MVN_HOME/bin/mvn --encrypt-password $artifactoryCloudPassword)
-
 	echo -e -n "\n${CYAN}Nexus (pkk82@nexus.pkk82.pl) password: ${NC}"
 	read -s nexusCloudPassword
 	encryptedNexusCloudPassword=$($MVN_HOME/bin/mvn --encrypt-password $nexusCloudPassword)
@@ -65,21 +61,6 @@ cat > $mvnSettings << EOL
 			<password>$encryptedDockerIoPassword</password>
 		</server>
 		<server>
-			<id>pkk82-artifactory-alibaba-cloud-public</id>
-			<username>pkk82</username>
-			<password>$encryptedArtifactoryCloudPassword</password>
-		</server>
-		<server>
-			<id>pkk82-artifactory-alibaba-cloud-release</id>
-			<username>pkk82</username>
-			<password>$encryptedArtifactoryCloudPassword</password>
-		</server>
-		<server>
-			<id>pkk82-artifactory-alibaba-cloud-snapshot</id>
-			<username>pkk82</username>
-			<password>$encryptedArtifactoryCloudPassword</password>
-		</server>
-		<server>
 			<id>pkk82-nexus</id>
 			<username>pkk82</username>
 			<password>$encryptedNexusCloudPassword</password>
@@ -96,42 +77,6 @@ cat > $mvnSettings << EOL
 		</server>
 	</servers>
 	<profiles>
-		<profile>
-			<id>repo-pkk82-artifactory-alibaba-cloud-public-anonymous</id>
-			<repositories>
-				<repository>
-					<id>pkk82-artifactory-alibaba-cloud-public-anonymous</id>
-					<url>http://47.91.91.114:8081/artifactory/pkk82-mvn-repo-public</url>
-				</repository>
-			</repositories>
-			<activation>
-				<activeByDefault>true</activeByDefault>
-			</activation>
-		</profile>
-		<profile>
-			<id>repo-pkk82-artifactory-alibaba-cloud-release</id>
-			<repositories>
-				<repository>
-					<id>pkk82-artifactory-alibaba-cloud-release</id>
-					<url>https://47.91.91.114:8444/artifactory/pkk82-mvn-repo-release</url>
-				</repository>
-			</repositories>
-			<activation>
-				<activeByDefault>true</activeByDefault>
-			</activation>
-		</profile>
-		<profile>
-			<id>repo-pkk82-artifactory-alibaba-cloud-snapshot</id>
-			<repositories>
-				<repository>
-					<id>pkk82-artifactory-alibaba-cloud-snapshot</id>
-					<url>https://47.91.91.114:8444/artifactory/pkk82-mvn-repo-snapshot</url>
-				</repository>
-			</repositories>
-			<activation>
-				<activeByDefault>true</activeByDefault>
-			</activation>
-		</profile>
 		<profile>
 			<id>repo-pkk82-nexus</id>
 			<repositories>
