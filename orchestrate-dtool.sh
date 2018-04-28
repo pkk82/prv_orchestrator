d1f4ed8f786bff21fc95d7f6ba882b5f487396b2
pkk82@macbook-pro <piotrkrzysztofkozak@gmail.com>
2018-04-28 10:31:48 +0200

    Fix bug related to unresolved variable

--- orchestrate-dtool.sh
+++ orchestrate-dtool.sh
@@ -2,7 +2,7 @@
 
 unzipFamily dtool
 
-latest=`ls -d $dtoolDir/dtool-* | sort | tail -n 1 2>/dev/null`
+latest=`ls -d $pfDir/dtool/dtool-* | sort | tail -n 1 2>/dev/null`
 echo "export DTOOL_HOME=$latest" | sed "s|$pfDir|\$PF_DIR|" >> $varFile
 echo "export PATH=\$DTOOL_HOME:\$PATH" >> $varFile
 
