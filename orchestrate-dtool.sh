47301c5e673f754b154747006a544aae3c3cddab
pkk82@macbook-pro <piotrkrzysztofkozak@gmail.com>
2018-04-28 10:14:43 +0200

    Simplify dtool orchestration

--- orchestrate-dtool.sh
+++ orchestrate-dtool.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-# copy dtool to pf
-dtoolDir=$pfDir/dtool
-makeDir $dtoolDir
 
-if [ `askYN "Configure dtool" "n"` == "y" ]; then
+unzipFamily dtool
 
-  unzipFamily dtool
-
-  latest=`ls -d $dtoolDir/dtool-* | sort | tail -n 1 2>/dev/null`
-  echo "export DTOOL_HOME=$latest" | sed "s|$pfDir|\$PF_DIR|" >> $varFile
-  echo "export PATH=\$DTOOL_HOME:\$PATH" >> $varFile
-
-fi
+latest=`ls -d $dtoolDir/dtool-* | sort | tail -n 1 2>/dev/null`
+echo "export DTOOL_HOME=$latest" | sed "s|$pfDir|\$PF_DIR|" >> $varFile
+echo "export PATH=\$DTOOL_HOME:\$PATH" >> $varFile
 
 
 
