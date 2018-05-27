d296020d5df3df5c1b343bc0fb84b1f3bef0bdbf
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-05-27 19:24:59 +0200

    Add intellij keys for settings and macro

--- intellij-idea/keymap.sh
+++ intellij-idea/keymap.sh
@@ -21,14 +21,23 @@ EOL
 
 cat > $intellijKeymapsDir/keymap-gnome.xml << EOL
 <keymap version="1" name="$gnomeName" parent="Default for GNOME">
+  <action id="CloseAllEditors">
+    <keyboard-shortcut first-keystroke="shift ctrl alt 4" />
+  </action>
   <action id="ImportModule">
     <keyboard-shortcut first-keystroke="shift ctrl alt o" />
   </action>
-  <action id="Maven.ReimportProject">
-    <keyboard-shortcut first-keystroke="shift ctrl alt r" />
+  <action id="Macro.format-on-save">
+   <keyboard-shortcut first-keystroke="ctrl alt s" />
   </action>
-  <action id="CloseAllEditors">
-    <keyboard-shortcut first-keystroke="shift ctrl alt 4" />
+  <action id="Maven.ReimportProject">
+   <keyboard-shortcut first-keystroke="shift ctrl alt r" />
+  </action>
+  <action id="ShowProjectStructureSettings">
+    <keyboard-shortcut first-keystroke="ctrl semicolon" />
+  </action>
+  <action id="ShowSettings">
+    <keyboard-shortcut first-keystroke="ctrl comma" />
   </action>
 </keymap>
 EOL
@@ -36,14 +45,17 @@ EOL
 
 cat > $intellijKeymapsDir/keymap-mac.xml << EOL
 <keymap version="1" name="$macName" parent="Mac OS X 10.5+">
+  <action id="CloseAllEditors">
+    <keyboard-shortcut first-keystroke="shift ctrl meta 4" />
+  </action>
   <action id="ImportModule">
     <keyboard-shortcut first-keystroke="shift ctrl meta o" />
   </action>
+  <action id="Macro.format-on-save">
+   <keyboard-shortcut first-keystroke="ctrl meta s" />
+  </action>
   <action id="Maven.ReimportProject">
     <keyboard-shortcut first-keystroke="shift ctrl meta r" />
-  </action>
-  <action id="CloseAllEditors">
-    <keyboard-shortcut first-keystroke="shift ctrl meta 4" />
   </action>
 </keymap>
 EOL
