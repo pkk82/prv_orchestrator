b34569bdc9956f76c1820507b49866be5df36cbe
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-06-02 11:56:19 +0200

    Add function displaying jar manifest

--- bash-functions/check-java-version.sh
+++ bash-functions/check-java-version.sh
@@ -1,27 +1,6 @@
-function check-java-version {
+function show-manifest {
   for jarFile in `ls -f ./*.jar 2>/dev/null`; do
-    className=`jar tf $jarFile | grep .class | grep -v [$] | head -n 1 | sed s/.class//g`
-    version=`javap -verbose -classpath $jarFile $className | grep 'major version:' | awk '{print $(NF)}'`
-    case $version in
-      52) javaVersion="8"
-        ;;
-      51) javaVersion="7"
-        ;;
-      50) javaVersion="6"
-        ;;
-      49) javaVersion="5"
-        ;;
-      48) javaVersion="1.4"
-        ;;
-      47) javaVersion="1.3"
-        ;;
-      46) javaVersion="1.2"
-        ;;
-      45) javaVersion="1.0 or 1.1"
-        ;;
-      *) javaVersion="unknown"
-        ;;
-    esac
-    echo "$jarFile - java version: $javaVersion"
+    echo "Displaying META-INF/MANIFEST.MF from $jarFile"
+    unzip -p $jarFile META-INF/MANIFEST.MF
   done
 }
