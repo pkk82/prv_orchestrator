f273254616a6b52dc71cc4700c978c9555186b79
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-06-09 13:13:56 +0200

    Show manifest for single file

--- bash-functions/show-manifest.sh
+++ bash-functions/show-manifest.sh
@@ -1,6 +1,11 @@
 function show-manifest {
-  for jarFile in `ls -f ./*.jar 2>/dev/null`; do
-    echo "Displaying META-INF/MANIFEST.MF from $jarFile"
-    unzip -p $jarFile META-INF/MANIFEST.MF
-  done
+  if [ -z "$1" ]; then
+    for jarFile in `ls -f ./*.jar 2>/dev/null`; do
+      echo "Displaying META-INF/MANIFEST.MF from $jarFile"
+      unzip -p $jarFile META-INF/MANIFEST.MF
+    done
+  else
+    echo "Displaying META-INF/MANIFEST.MF from $1"
+    unzip -p $1 META-INF/MANIFEST.MF
+  fi
 }
