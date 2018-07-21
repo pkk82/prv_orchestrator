f537995d9e54fe33d2c806b46997afd1b42fe20e
pkk82@l2015-mac <pkk82@l2015-mac>
2018-07-21 11:46:29 +0200

    Create keymap in intellij for windows

--- intellij-idea/keymap.sh
+++ intellij-idea/keymap.sh
@@ -4,11 +4,14 @@ keymapFile=$intellijOptionsDir/keymap.xml
 
 gnomeName="keymap-gnome"
 macName="keymap-mac"
+winName="keymap-win"
 
 if [ "$system" == "linux" ]; then
   name=$gnomeName
 elif [ "$system" == "mac"  ]; then
   name=$macName
+elif [ "$system" == "windows" ]; then
+  name=$winName
 fi
 
 cat > $keymapFile << EOL
@@ -52,6 +55,38 @@ cat > $intellijKeymapsDir/keymap-gnome.xml << EOL
 </keymap>
 EOL
 
+cat > $intellijKeymapsDir/keymap-win.xml << EOL
+<keymap version="1" name="$winName" parent="\$default">
+  <action id="CloseAllEditors">
+    <keyboard-shortcut first-keystroke="shift ctrl alt 4" />
+  </action>
+  <action id="DatabaseView.OpenDdlInConsole" />
+  <action id="ImportModule">
+    <keyboard-shortcut first-keystroke="shift ctrl alt o" />
+  </action>
+  <action id="Macro.format-on-save">
+   <keyboard-shortcut first-keystroke="ctrl alt s" />
+  </action>
+  <action id="Maven.ReimportProject">
+   <keyboard-shortcut first-keystroke="shift ctrl alt r" />
+  </action>
+  <action id="OpenInBrowser">
+    <keyboard-shortcut first-keystroke="shift ctrl alt b" />
+  </action>
+  <action id="ShowProjectStructureSettings">
+    <keyboard-shortcut first-keystroke="ctrl semicolon" />
+  </action>
+  <action id="ShowSettings">
+    <keyboard-shortcut first-keystroke="ctrl comma" />
+  </action>
+  <action id="Back">
+    <keyboard-shortcut first-keystroke="ctrl alt left" />
+  </action>
+  <action id="Forward">
+    <keyboard-shortcut first-keystroke="ctrl alt right" />
+  </action>
+</keymap>
+EOL
 
 cat > $intellijKeymapsDir/keymap-mac.xml << EOL
 <keymap version="1" name="$macName" parent="Mac OS X 10.5+">
