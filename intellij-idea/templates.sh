17ba0823b48b4f1c3c1be503619f7037e9551d44
pkk82@l2015-mac <piotrkrzysztofkozak@gmail.com>
2018-09-30 22:58:51 +0200

    Fix template description

--- intellij-idea/templates.sh
+++ intellij-idea/templates.sh
@@ -4,11 +4,10 @@ javaScriptXmlFile=$intellijTemplatesDir/JavaScript.xml
 
 cat > $javaScriptXmlFile << EOL
 <templateSet group="JavaScript">
-  <template name="iife" value="(function () {&#10;    'use strict';&#10;    $END$&#10;}());" description="Inserts  immediately-invoked function expression" toReformat="true" toShortenFQNames="true">
+  <template name="iife" value="(function () {&#10;    'use strict';&#10;    $END$&#10;}());" description="Inserts immediately-invoked function expression" toReformat="true" toShortenFQNames="true">
     <context>
       <option name="JS_STATEMENT" value="true" />
     </context>
   </template>
 </templateSet>
 EOL
-
