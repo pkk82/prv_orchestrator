78e7492ec81bea25ce048c6b3edcc9b28c830533
piotr@d2018-win <piotrkrzysztofkozak@gmail.com>
2018-10-27 09:38:59 +0200

    Turn off warning for simple variable usage in bash

--- intellij-idea/inspections.sh
+++ intellij-idea/inspections.sh
@@ -5,6 +5,7 @@ inspectionsFile=$intellijInspectionDir/pkk82.xml
 cat > $inspectionsFile << EOL
 <profile version="1.0">
   <option name="myName" value="pkk82" />
+  <inspection_tool class="BashSimpleVarUsage" enabled="true" level="INFORMATION" enabled_by_default="true" />
   <inspection_tool class="WeakerAccess" enabled="true" level="WEAK WARNING" enabled_by_default="true">
     <option name="SUGGEST_PACKAGE_LOCAL_FOR_MEMBERS" value="true" />
     <option name="SUGGEST_PACKAGE_LOCAL_FOR_TOP_CLASSES" value="true" />
