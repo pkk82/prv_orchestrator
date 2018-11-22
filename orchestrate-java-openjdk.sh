e896c2ba90bf7f169325c48a2b20541b607e5f26
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-11-22 21:35:08 +0100

    Make openjdk provisioning similar to java

--- orchestrate-java-openjdk.sh
+++ orchestrate-java-openjdk.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 untarFamily java-openjdk "" "sed 's/-$system//g'"
-verifyVersion java-openjdk "bin/java -version 2>&1 | grep 'Runtime' | awk '{print \$NF}' | sed 's/\+/u/g' | sed 's/1\.//g' | sed 's/\.0_/u/g' | sed 's/)//g' | sed 's/-b[0-9]*//g'"
-createVariables2 java-openjdk openjdk "sed 's/openjdk-//g' | sed 's/u/./g' | sed 's/-x64//g'"
+
+verify java-openjdk \
+  "awk -F- '{print \$(NF-1)}' | sed 's/^1\.//g'" \
+  "bin/java -version 2>&1 | grep 'Runtime' | awk '{print \$NF}' | sed 's/\+/u/g' | sed 's/1\.//g' | sed 's/\.0_/u/g' | sed 's/)//g' | sed 's/-b[0-9]*//g'" \
+  "version"
+
+createVariables java-openjdk openjdk \
+  "awk -F- '{print \$(NF-1)}' | sed 's/1\.//g' | cut -du -f1 | cut -d. -f1" \
+  "awk -F- '{print \$NF}'"
