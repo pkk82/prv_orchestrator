793054bb502d78ae559bc5e405ca42161117f92e
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-11-26 20:41:09 +0100

    Add scripts installing and configuring docker

--- orchestrate-ubuntu.sh
+++ orchestrate-ubuntu.sh
@@ -62,3 +62,24 @@ sudo apt-get install virtualbox-5.2
 
 # install gnu-smalltalk
 sudo apt install gnu-smalltalk
+
+# install docker
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# allow user to run docker
+sudo groupadd docker
+sudo usermod -aG docker pkk82
+sudo chown "pkk82":"pkk82" /home/pkk82/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+
+# run docker on startup
+sudo systemctl enable docker
