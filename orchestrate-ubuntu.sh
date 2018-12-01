4ce7d8c16341025973065062062f0dcaa93f3414
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-12-01 17:12:25 +0100

    Show how to install minikube

--- orchestrate-ubuntu.sh
+++ orchestrate-ubuntu.sh
@@ -87,3 +87,5 @@ sudo systemctl enable docker
 # install kubernetes
 sudo snap install kubectl --classic
 
+# install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
