66d9bf753619b44038f75a4b6382f3dfe5809b60
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-12-15 14:59:22 +0100

    Add xclip to ubuntu orchestration

--- orchestrate-ubuntu.sh
+++ orchestrate-ubuntu.sh
@@ -89,3 +89,6 @@ sudo snap install kubectl --classic
 
 # install minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+
+# install xclip
+sudo apt install xclip
