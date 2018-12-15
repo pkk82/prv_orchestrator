c3628242726abcfd87d747413c7314828a6af6fb
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-12-15 13:09:49 +0100

    Create intellij settings for all home dirs

--- orchestrate-intellij-idea-settings.sh
+++ orchestrate-intellij-idea-settings.sh
@@ -1,40 +1,36 @@
 #!/usr/bin/env bash
 
-existingDirs=`ls -d $HOME/.IntelliJIdea* $HOME/.intellij-idea* $HOME/Library/Preferences/IntelliJIdea* 2>/dev/null`
-intellijHomeDirDefault=`ls -d $HOME/.IntelliJIdea* $HOME/.intellij-idea* $HOME/Library/Preferences/IntelliJIdea* 2>/dev/null | sort | tail -n 1`
+intellijHomeDirs=`ls -d $HOME/.IntelliJIdea* $HOME/.intellij-idea* $HOME/Library/Preferences/IntelliJIdea* 2>/dev/null`
 
-echo "Existing Intellij Idea directories:"
-echo "$existingDirs"
-if [ "$intellijHomeDirDefault" == "" ]; then
+if [[ "$intellijHomeDirs" == "" ]]; then
   echo -e -n "${CYAN}Enter path to intellij home directory${NC}: "
-else
-  echo -e -n "${CYAN}Enter path to intellij home directory${NC} ($intellijHomeDirDefault): "
+  read newIntellijHomeDir
+  makeDir $newIntellijHomeDir
+  intellijHomeDirs=($newIntellijHomeDir)
 fi
 
-read intellijHomeDir
-intellijHomeDir=${intellijHomeDir:-$intellijHomeDirDefault}
-makeDir $intellijHomeDir
+for intellijHomeDir in $intellijHomeDirs; do
 
-if [ "$system" = "mac" ]; then
-  intellijWorkingDir=$intellijHomeDir
-else
-  intellijWorkingDir=$intellijHomeDir/config
-fi
+  if [[ "$system" = "mac" ]]; then
+    intellijWorkingDir=$intellijHomeDir
+  else
+    intellijWorkingDir=$intellijHomeDir/config
+  fi
 
-intellijOptionsDir=$intellijWorkingDir/options
-makeDir $intellijOptionsDir
+  intellijOptionsDir=$intellijWorkingDir/options
+  makeDir $intellijOptionsDir
 
-intellijInspectionDir=$intellijWorkingDir/inspection
-makeDir $intellijInspectionDir
+  intellijInspectionDir=$intellijWorkingDir/inspection
+  makeDir $intellijInspectionDir
 
-intellijKeymapsDir=$intellijWorkingDir/keymaps
-makeDir $intellijKeymapsDir
+  intellijKeymapsDir=$intellijWorkingDir/keymaps
+  makeDir $intellijKeymapsDir
 
-intellijTemplatesDir=$intellijWorkingDir/templates
-makeDir $intellijTemplatesDir
+  intellijTemplatesDir=$intellijWorkingDir/templates
+  makeDir $intellijTemplatesDir
 
-makeDir $intellijOptionsDir
+  for shFile in `ls ./intellij-idea/*.sh`; do
+    . $shFile
+  done
 
-for shFile in `ls ./intellij-idea/*.sh`; do
-  . $shFile
 done
