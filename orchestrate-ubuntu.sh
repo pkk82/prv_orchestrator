01eeb4ce06fe0d2cf1c8e2de50e4be89846bcc7c
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2018-12-17 20:09:19 +0100

    Fix typos

--- orchestrate-ubuntu.sh
+++ orchestrate-ubuntu.sh
@@ -8,7 +8,7 @@ sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
 
-# install languate packates
+# install language packages
 sudo apt-get install hunspell-en-gb
 sudo apt-get install hunspell-pl
 
