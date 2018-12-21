f5fae2ee784f27bff77c0a3af0b667e66644a5e0
pkk82@l2015-mac <piotrkrzysztofkozak@gmail.com>
2018-12-21 12:08:10 +0100

    Try to find customer secret in previous versions

--- orchestrate-dtool.sh
+++ orchestrate-dtool.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
+files=(dtool.sh dtool.bat dtool.command)
+keyValue="\-Ddtool.bitbucket.customer.secret"
+customerSecret=""
+
+# find existing customer key
+for dtool in `ls -d $pfDir/dtool/dtool-* 2>/dev/null`; do
+  for file in ${files[*]}; do
+    launcher="$dtool/$file"
+    if grep -q "$keyValue=" "$launcher"; then
+      if ! grep -q "$keyValue=\.\.\." "$launcher"; then
+        customerSecret=`cat $launcher | grep $keyValue | awk '{print $1}' | cut -d= -f2`
+        break 2
+      fi
+    fi
+  done
+done
+
+# create new
 unzipFamily dtool
 
 latest=`ls -d $pfDir/dtool/dtool-* | sort | tail -n 1 2>/dev/null`
 echo "export DTOOL_HOME=$latest" | sed "s|$pfDir|\$PF_DIR|" >> $varFile
 echo "export PATH=\$DTOOL_HOME:\$PATH" >> $varFile
 
-files=(dtool.sh dtool.bat dtool.command)
-keyValue="\-Ddtool.bitbucket.customer.secret"
-customerSecret=""
 for dtool in `ls -d $pfDir/dtool/dtool-* 2>/dev/null`; do
   for file in ${files[*]}; do
     launcher="$dtool/$file"
