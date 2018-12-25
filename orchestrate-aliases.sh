dde5fbff46b6dddd7da613bfc18f6adbee56b45b
pkk82@l2015-mac <piotrkrzysztofkozak@gmail.com>
2018-12-25 16:16:11 +0100

    Update maven wrapper before use

--- orchestrate-aliases.sh
+++ orchestrate-aliases.sh
@@ -7,6 +7,6 @@ alias dra='docker ps -a -q | xargs docker rm'
 alias dria='docker images -q | xargs docker rmi -f'
 alias dris='docker images | grep SNAPSHOT | awk '\''{print \$3}'\'' | xargs docker rmi -f'
 alias cclip='xclip -selection clipboard'
-alias use-maven-wrapper='mvn -N io.takari:maven:wrapper'
+alias use-maven-wrapper='mvn -U -N io.takari:maven:wrapper'
 alias ssh-nexus='ssh pkk82.pl -p 57185'
 EOL
