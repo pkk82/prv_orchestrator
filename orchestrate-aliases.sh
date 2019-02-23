8849e8453ef6ffa5b8c2a0e846eb7801c8da8697
pkk82@l2015-mac <piotrkrzysztofkozak@gmail.com>
2019-02-23 22:05:25 +0800

    Modify mac alias for clipboard

--- orchestrate-aliases.sh
+++ orchestrate-aliases.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 aliases=~/.bash_aliases
 
+if [[ "$system" == "mac" ]]; then
+  cclip="alias cclip='pbcopy'"
+else
+  cclip="alias cclip='xclip -selection clipboard'"
+fi
+
 cat > $aliases << EOL
 alias dsa='docker ps -q | xargs docker stop'
 alias dra='docker ps -a -q | xargs docker rm'
 alias dria='docker images -q | xargs docker rmi -f'
 alias dris='docker images | grep SNAPSHOT | awk '\''{print \$3}'\'' | xargs docker rmi -f'
-alias cclip='xclip -selection clipboard'
 alias use-maven-wrapper='mvn -U -N io.takari:maven:wrapper'
 alias ssh-nexus='ssh pkk82.pl -p 57185'
+$cclip
 EOL
