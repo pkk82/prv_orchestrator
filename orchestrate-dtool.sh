6757bed100d859797e3c71830dbc41a8ad0f2653
pkk82@l2015-mac <piotrkrzysztofkozak@gmail.com>
2019-03-02 19:21:50 +0100

    Ask for customer key when orchestrating dtool

--- orchestrate-dtool.sh
+++ orchestrate-dtool.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
 files=(dtool.sh dtool.bat dtool.command)
-keyValue="\-Ddtool.bitbucket.customer.secret"
-customerSecret=""
 
-# find existing customer key
-for dtool in `ls -d $pfDir/dtool/dtool-* 2>/dev/null`; do
-  for file in ${files[*]}; do
-    launcher="$dtool/$file"
-    if grep -q "$keyValue=" "$launcher"; then
-      if ! grep -q "$keyValue=\.\.\." "$launcher"; then
-        customerSecret=`cat $launcher | grep $keyValue | awk '{print $1}' | cut -d= -f2`
-        break 2
+# find key
+function findKey {
+  value=""
+  for dtool in `ls -d $pfDir/dtool/dtool-* 2>/dev/null`; do
+    for file in ${files[*]}; do
+      launcher="$dtool/$file"
+      if grep -q "$1=" "$launcher"; then
+        if ! grep -q "$1=\.\.\." "$launcher"; then
+          value=`cat $launcher | grep $1 | awk '{print $1}' | cut -d= -f2`
+          break 2
+        fi
       fi
-    fi
+    done
   done
-done
+  echo "$value"
+}
+
+customerSecretKey="\-Ddtool.bitbucket.customer.secret"
+customerSecret=`findKey $customerSecretKey`
+
+customerKeyKey="\-Ddtool.bitbucket.customer.key"
+customerKey=`findKey $customerKeyKey`
+
 
 # create new
 unzipFamily dtool
@@ -27,13 +36,20 @@ echo "export PATH=\$DTOOL_HOME:\$PATH" >> $varFile
 for dtool in `ls -d $pfDir/dtool/dtool-* 2>/dev/null`; do
   for file in ${files[*]}; do
     launcher="$dtool/$file"
-    if grep -q "$keyValue=\.\.\." "$launcher"; then
+    if grep -q "$customerSecretKey=\.\.\." "$launcher"; then
+      if [[ "$customerKey" == "" ]]; then
+        customerKey=`askPassword "Enter bitbucket dtool key"`
+        printf "\n"
+      fi
+       if [[ "$customerKey" != "" ]]; then
+        sed -i $sedBackupSuffix "s/$customerKeyKey=\.\.\./$customerKeyKey=$customerKey/g" "$launcher"
+      fi
       if [[ "$customerSecret" == "" ]]; then
         customerSecret=`askPassword "Enter bitbucket dtool secret"`
         printf "\n"
       fi
       if [[ "$customerSecret" != "" ]]; then
-        sed -i $sedBackupSuffix "s/$keyValue=\.\.\./$keyValue=$customerSecret/g" "$launcher"
+        sed -i $sedBackupSuffix "s/$customerSecretKey=\.\.\./$customerSecretKey=$customerSecret/g" "$launcher"
       fi
     fi
   done
