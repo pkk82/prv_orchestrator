703ddc003e6639a26e489e26f5faa77c80935d08
pkk82@l2015-mac <piotrkrzysztofkozak@gmail.com>
2019-03-23 10:14:32 +0100

    Separate dtool section in .bash_variables

--- orchestrate-dtool.sh
+++ orchestrate-dtool.sh
@@ -48,6 +48,7 @@ function replaceInFile {
 unzipFamily dtool
 
 latest=`ls -d $pfDir/dtool/dtool-* | sort | tail -n 1 2>/dev/null`
+echo "# dtool" >> "$varFile"
 echo "export DTOOL_HOME=$latest" | sed "s|$pfDir|\$PF_DIR|" >> $varFile
 echo "export PATH=\$DTOOL_HOME:\$PATH" >> $varFile
 
