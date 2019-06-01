6f317886fcb5622b8a5f644c0c80cf46c0d929f6
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2019-06-01 10:57:36 +0200

    Add intellij templates

--- intellij-idea/templates.sh
+++ intellij-idea/templates.sh
@@ -21,5 +21,29 @@ cat > $surroundXmlFile << EOL
       <option name="JAVA_STATEMENT" value="true" />
     </context>
   </template>
+   <template name="QM" value="&quot;\$SELECTION\$&quot;" shortcut="NONE" description="Surround with &quot;&quot;" toReformat="false" toShortenFQNames="true">
+    <context>
+      <option name="OTHER" value="true" />
+    </context>
+  </template>
+  <template name="A" value="'\$SELECTION\$'" shortcut="NONE" description="Surround with ''" toReformat="false" toShortenFQNames="true">
+    <context>
+      <option name="OTHER" value="true" />
+    </context>
+  </template>
+</templateSet>
+EOL
+
+
+ownXmlFile=$intellijTemplatesDir/own.xml
+cat > $ownXmlFile << EOL
+<templateSet group="own">
+  <template name="slf4j" value="private final static org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(\$PACKAGE\$.\$CLASS\$.class);\$END\$" description="static slf4j logger" toReformat="false" toShortenFQNames="true">
+    <variable name="PACKAGE" expression="currentPackage()" defaultValue="" alwaysStopAt="true" />
+    <variable name="CLASS" expression="className()" defaultValue="" alwaysStopAt="true" />
+    <context>
+      <option name="JAVA_DECLARATION" value="true" />
+    </context>
+  </template>
 </templateSet>
 EOL
