60edfafa30275ff88a3f184f1de576f20a0bdb2e
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2019-06-12 20:32:14 +0200

    Add function copying node submodule

---
+++ bash-functions/copy-node-submodule.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function copy-node-submodule {
+
+  if [[ detect-system == "mac" ]]; then
+    sedBackupSuffix=".bak"
+  else
+    sedBackupSuffix=""
+  fi
+
+  if [[ "$1" == ""  || "$2" == "" ]]; then
+    echo "Use create-gradle-submodule <src module name> <dest module name>"
+    return 1;
+  fi
+
+  if [[ ! -d "./$1" ]]; then
+    echo "$2 must be a directory in current folder"
+    return 1;
+  fi
+
+  mkdir -p $2
+  cp -a $1/. $2/
+  rm -rf $2/node_modules
+  rm -f $2/package-lock.json
+
+  sed -i $sedBackupSuffix "s/\"name\": \".*\"/\"name\": \"$2\"/g" "$2/package.json"
+}
